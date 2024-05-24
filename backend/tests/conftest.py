d8236ca0db674e0789d9e985e27962914d5b1b75
Damian Malik <malikdamiann@gmail.com>
2024-05-24 22:35:15 +0200

    create fixture with product result

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,9 +3,9 @@ from typing import Any, Generator
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
-from backend.database import Base, get_db
+from backend.database import Base, get_db, ProductResult
 from backend.main import app
 
 DATABASE_URL = "sqlite:///./test_db.db"
@@ -13,7 +13,7 @@ engine = create_engine(DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 
 def override_get_db():
@@ -34,3 +34,33 @@ app.dependency_overrides[get_db] = override_get_db
 def client() -> Generator[TestClient, Any, None]:
     with TestClient(app) as client:
         yield client
+
+
+@pytest.fixture
+def session() -> Generator[Session, Any, Any]:
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    db = TestingSessionLocal()
+    try:
+        yield db
+    except Exception as exc:
+        db.rollback()
+        raise exc
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def submit_product(session: Session) -> ProductResult:
+    product_result = ProductResult(
+        name="Test-product",
+        url="www.test.com",
+        image="test.png",
+        price=120.22,
+        search_text="test-text",
+        source="test-source",
+    )
+    session.add(product_result)
+    session.commit()
+    return product_result
