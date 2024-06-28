b0cc032e31546e54a73f78207934fc444bc370b4
Damian Malik <malikdamiann@gmail.com>
2024-06-28 22:43:20 +0200

    feat: add update_tracked_products function with error handling and logging

--- backend/api/tracked_product.py
+++ backend/api/tracked_product.py
@@ -1,3 +1,5 @@
+import logging
+import subprocess
 from typing import Annotated, Sequence
 
 from fastapi import APIRouter, Depends, HTTPException, Path, Query, status
@@ -5,7 +7,10 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from backend import schemas
+from backend.config import settings
 from backend.database import TrackedProducts, get_db
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(tags=["Tracked Product"])
 
@@ -65,3 +70,41 @@ def toggle_tracked_product(
 def get_tracked_products(db: Annotated[Session, Depends(get_db)]) -> Sequence[TrackedProducts]:
     """Retrieve all tracked products from the database."""
     return db.execute(select(TrackedProducts)).scalars().all()
+
+
+@router.post(
+    "/update-tracked-products",
+    status_code=status.HTTP_200_OK,
+    response_model=dict[str, str | list[str]],
+    summary="Updated tracked products",
+)
+def update_tracked_products(db: Annotated[Session, Depends(get_db)]) -> dict[str, str | list[str]]:
+    """Starts scrapers for all tracked products."""
+    query = select(TrackedProducts).where(TrackedProducts.tracked.is_(True))
+    tracked_products = db.execute(query).scalars().all()
+
+    if not tracked_products:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No tracked product found"
+        )
+
+    url = settings.AMAZON_URL
+
+    product_names: list[str] = []
+    for tracked_product in tracked_products:
+        name = tracked_product.name
+
+        command = f"python ./scraper/__init__.py {url} \"{name}\" /results"
+
+        try:
+            subprocess.Popen(command, shell=True)
+        except subprocess.SubprocessError as e:
+            logger.error(f"Failed tos tart scraper for {name}: {e}")
+            continue
+
+        product_names.append(name)
+
+    return {
+        "message": "Scrapers started successfully",
+        "products": product_names
+    }
