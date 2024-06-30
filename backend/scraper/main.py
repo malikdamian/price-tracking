9e54addc164fb7a65f98a876a18d806ac0a95732
Damian Malik <malikdamiann@gmail.com>
2024-06-30 10:13:18 +0200

    feat: search products using playwright

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -1,5 +1,27 @@
-from backend.scraper.utils import load_auth
+from playwright.async_api import Page
+
+from backend.scraper.utils import load_auth, logger
 
 credential = load_auth()
 auth = f"{credential["username"]}:{credential["password"]}"
 browser_url = f"wss://{auth}@{credential["host"]}"
+
+
+async def search(metadata: dict[str, str], page: Page, search_text: str) -> Page:
+    logger.info(f"Searching for {search_text} on {page.url}")
+
+    search_field_query = metadata.get("search_field_query")
+    search_button_query = metadata.get("search_button_query")
+
+    if not search_field_query or not search_button_query:
+        raise Exception("Could not search")
+
+    logger.info("Filling input field")
+    search_box = await page.wait_for_selector(search_field_query)
+    await search_box.type(search_text)
+    logger.info("Pressing search button")
+    button = await page.wait_for_selector(search_button_query)
+    await button.click()
+
+    await page.wait_for_load_state()
+    return page
